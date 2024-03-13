7ab16c46970ef252a786ead054c4d384d704e5c8
vishnu kushwaha <getch.talk21@gmail.com>
2024-03-14 02:09:48 +0530

    update

--- byvishnu.js
+++ byvishnu.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+  // var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  
+  // $(document).ready(()=>{
+  //   $.getJSON("https://api.ipify.org?format=json",
+  //   function (data) {
+
+  //       // Displayin IP address on screen
+  //       // alert(data.ip);
+  //   });
+
+// });
+
+
+
+
     // Function to update the current time
     function updateCurrentTime() {
       var currentTime = new Date();
@@ -7,7 +22,7 @@ $(document).ready(function() {
       var seconds = currentTime.getSeconds();
 
       // Convert hours to 12-hour format and determine AM/PM
-      var ampm = hours >= 12 ? 'AM' : 'PM';
+      var ampm = hours >= 12 ? 'PM' : 'AM';
       hours = hours % 12;
       hours = hours ? hours : 12; // Handle midnight (0 hours)
 
@@ -40,6 +55,10 @@ $(document).ready(function() {
       // Toggle between classes
       if (bodyClasses === 'bg-black text-white') {
           $('body').removeClass('bg-black text-white').addClass('bg-white text-black');
+          $('body').css({
+            'font-weight': 1000
+          });
+          
       } else {
           $('body').removeClass('bg-white text-black').addClass('bg-black text-white');
       }
@@ -63,6 +82,7 @@ $(document).ready(function() {
     // }
 
     $(document).ready(function() {
+      $('#circularcursor').html('<span style="width:5px; height:5px; background-color:red; border-radius:50px;  position:absolute;"></span>');
       $(document).on('mousemove', function(e) {
         $('#circularcursor').css({
           left: e.pageX,
